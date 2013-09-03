6680deb2dff79ced156ffe8e902484a6ed91106e
Will Webberley <willwebberley@gmail.com>
2013-09-03 23:36:48 +0100

    fixed host scanning

--- master/server.py
+++ master/server.py
@@ -1,17 +1,29 @@
 from flask import Flask, url_for, render_template, request, session, escape, redirect, g
-import os, nmap, subprocess, signal
+import os, nmap, subprocess, signal, urllib2
 
 app = Flask(__name__)
 
 
 def search():
     nm = nmap.PortScanner()
-    nm.scan(hosts='192.168.1.4', arguments='-p 9875')
-    hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
-    
-    for host, status in hosts_list:
-        print('{0}:{1}'.format(host, status))
-    return hosts_list
+    nm.scan(hosts='192.168.1.0-10', arguments='-p 9875')
+   
+    up_hosts = [] 
+    for h in nm.all_hosts():
+    #    print h,nm[h]['tcp'][9875]['state']
+        if nm[h]['tcp'][9875]['state'] == 'open':
+            up_hosts.append(str(h))
+
+    zone_dict = {}
+    for host in up_hosts:
+        try:
+            response = urllib2.urlopen('http://'+host+":"+str(9875)+"/info")
+            zone = response.read()
+            zone_dict[host] = zone
+        except Exception as e:
+            print "error:",host, e
+
+    return zone_dict
 
 
 def saveIds(mod1, mod2, pid):
@@ -58,20 +70,63 @@ def endStream():
 
 
 def redirectInputs():
-    #first, get the id of the casastream RTP sink:
+    # First, get the id of the casastream RTP sink
+    pa_sinks_process = subprocess.Popen(["pactl","list","short","sinks"], stdout = subprocess.PIPE)
+    out1, err1 = pa_sinks_process.communicate()
+    lines = out1.split("\n")
+    sink_id = 0
+    for line in lines:
+        tokens = line.split()
+        for token in tokens:
+            if token == 'rtp':
+                sink_id = tokens[0]
+
+
+    # Next, get the IDs of all current inputs
+    pa_inputs_process = subprocess.Popen(["pactl","list","short","sink-inputs"], stdout = subprocess.PIPE)
+    out2, err2 = pa_inputs_process.communicate()
+    lines = out2.split("\n")
+    inputs = []
+    for line in lines:
+        tokens = line.split()
+        if len(tokens) > 0:
+            inputs.append(tokens[0])
     
- 
+    # Finally, move each input to our own sink
+    for i in inputs:
+        subprocess.Popen(["pactl","move-sink-input",str(i),str(sink_id)])
+        
+
+
 @app.route("/")
 def home():
-#    stuff = search()
-    startStream()
     return "test"
 
+@app.route("/scan/", methods=["POST", "GET"])
+def scan():
+    zones = search()
+    text = ""
+    for zone in zones:
+        text = text + zone + ":" + zones[zone]+"<br />"
+    return text
 
+@app.route("/start/", methods=["POST", "GET"])
+def start():
+    startStream()
+    return "started"
+
+@app.route("/stop/", methods=["POST", "GET"])
+def stop():
+    endStream()
+    return "stopped"
+
+@app.route("/sort-inputs/", methods=["POST", "GET"])
+def stop():
+    redirectInputs()
 
 
 # Main code (if invoked from Python at command line for development server)
 if __name__ == '__main__':
     app.debug = True 
-    port = int(os.environ.get('PORT', 9878))
+    port = 9878
     app.run(host='0.0.0.0', port=port)
